e0792c984da72d269b0b38cdaa44e1a4317e0928
Darnell O <DarnellOtto@ac.c4q.nyc>
2017-11-29 00:46:03 -0500

    first commit

--- app/src/main/java/com/example/rusili/homework11/pokedexActivity/model/Pokedex.java
+++ app/src/main/java/com/example/rusili/homework11/pokedexActivity/model/Pokedex.java
@@ -15,4 +15,19 @@ public class Pokedex {
     private PokemonEntries[] pokemon_entries;
 
     //TODO: Create getters
+
+    public Pokedex(int id, Region region, VersionGroups[] version_groups, PokemonEntries[] pokemon_entries) {
+        this.id = id;
+        this.region = region;
+
+        this.version_groups = version_groups;
+        this.pokemon_entries = pokemon_entries;
+    }
+
+
+
+
+
+
+
 }
