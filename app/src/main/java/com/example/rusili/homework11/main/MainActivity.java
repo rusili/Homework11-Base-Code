ec7858c2305f0738029e9294ca05f40b547fad1e
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2017-11-29 18:03:40 -0500

    filled in empty paramter to ensure succesful build, layouts have to be replaced

---
+++ app/src/main/java/com/example/rusili/homework11/main/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rusili.homework11.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rusili.homework11.R;
+
+/**
+ * Created by Shant on 11/29/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
