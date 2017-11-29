0c77c0fc8f7c901c516fd82ea5c45100e1cefc52
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-11-28 21:06:24 -0500

    initial commit

--- app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
@@ -3,18 +3,22 @@ package com.example.rusili.homework11.detailscreen.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.rusili.homework11.R;
 import com.example.rusili.homework11.detailscreen.model.Pokemon;
 import com.example.rusili.homework11.network.RetrofitFactory;
 
 public class PokemonDetailActivity extends AppCompatActivity{
 	private RetrofitFactory.PokemonNetworkListener pokemonNetworkListener;
-
+ 	private ImageView pokemonPic;
+ 	private String pokemonName;
 	@Override
 	public void onCreate (@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView();
+		setContentView(R.layout.detail_activity);
+		pokemonPic = findViewById(R.id.pokemon_pic);
 
 		initialize();
 	}
@@ -28,6 +32,10 @@ public class PokemonDetailActivity extends AppCompatActivity{
 			@Override
 			public void pokemonCallback (Pokemon pokemon) {
 				//TODO: Display pokemon data
+				Glide.with(getApplicationContext())
+						.load("http://via.placeholder.com/300.png")
+						.override(300, 200)
+						.into(pokemonPic);
 				//Hint: Learn how to use Glide to display an image.
 			}
 		};
