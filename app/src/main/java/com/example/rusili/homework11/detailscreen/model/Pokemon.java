758e57817862d0bbb5c56df7599879afd21b7538
osibo00 <olegsiboglov@ac.c4q.nyc>
2017-11-29 20:35:21 -0500

    recyclerview initialized

--- app/src/main/java/com/example/rusili/homework11/detailscreen/model/Pokemon.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/model/Pokemon.java
@@ -10,4 +10,29 @@ public class Pokemon {
 	private Types[] types;
 
 	//TODO: Create getters
+
+
+    public Stats[] getStats() {
+        return stats;
+    }
+
+    public void setStats(Stats[] stats) {
+        this.stats = stats;
+    }
+
+    public Sprites getSprites() {
+        return sprites;
+    }
+
+    public void setSprites(Sprites sprites) {
+        this.sprites = sprites;
+    }
+
+    public Types[] getTypes() {
+        return types;
+    }
+
+    public void setTypes(Types[] types) {
+        this.types = types;
+    }
 }
--- app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
@@ -1,37 +1,40 @@
-package com.example.rusili.homework11.detailscreen.view;
+//package com.example.rusili.homework11.detailscreen.view;
+//
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+//
+//import com.example.rusili.homework11.R;
+//import com.example.rusili.homework11.detailscreen.model.Pokemon;
+//import com.example.rusili.homework11.network.RetrofitFactory;
+//
+//public class PokemonDetailActivity extends AppCompatActivity{
+//	private RetrofitFactory.PokemonNetworkListener pokemonNetworkListener;
+//	private String pokemonName;
+//
+//	@Override
+//	public void onCreate (@Nullable Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView();
+//
+//		initialize();
+//	}
+//
+//	private void initialize () {
+//		getPokemonDetails();
+//	}
+//
+//	private void getPokemonDetails () {
+//		pokemonNetworkListener = new RetrofitFactory.PokemonNetworkListener() {
+//			@Override
+//			public void pokemonCallback (Pokemon pokemon) {
+//				//TODO: Display pokemon data
+//				//Hint: Learn how to use Glide to display an image.
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
-import com.example.rusili.homework11.R;
-import com.example.rusili.homework11.detailscreen.model.Pokemon;
-import com.example.rusili.homework11.network.RetrofitFactory;
-
-public class PokemonDetailActivity extends AppCompatActivity{
-	private RetrofitFactory.PokemonNetworkListener pokemonNetworkListener;
-
-	@Override
-	public void onCreate (@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView();
-
-		initialize();
-	}
-
-	private void initialize () {
-		getPokemonDetails();
-	}
-
-	private void getPokemonDetails () {
-		pokemonNetworkListener = new RetrofitFactory.PokemonNetworkListener() {
-			@Override
-			public void pokemonCallback (Pokemon pokemon) {
-				//TODO: Display pokemon data
-				//Hint: Learn how to use Glide to display an image.
-			}
-		};
-		RetrofitFactory.getInstance().setPokemonNetworkListener(pokemonNetworkListener);
-		RetrofitFactory.getInstance().getPokemon(pokemonName);
-	}
-}
+//			}
+//		};
+//		RetrofitFactory.getInstance().setPokemonNetworkListener(pokemonNetworkListener);
+//		RetrofitFactory.getInstance().getPokemon(pokemonName);
+//	}
+//}
