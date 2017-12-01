5fd714e6219b2fe0d55665725ff5f06b45957cdf
rusili <>
2017-12-01 16:56:23 -0500

    Shows snackbar when network call fails.

--- app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.rusili.homework11.detailscreen.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.rusili.homework11.R;
@@ -29,6 +30,11 @@ public class PokemonDetailActivity extends AppCompatActivity{
 			public void pokemonCallback (Pokemon pokemon) {
 				//TODO: Display pokemon data
 				//Hint: Learn how to use Glide to display an image.
+			}
+
+			@Override
+			public void onNetworkError(Throwable t) {
+				Snackbar.make(findViewById(android.R.id.content), t.getMessage(), Snackbar.LENGTH_LONG).show();
 			}
 		};
 		RetrofitFactory.getInstance().setPokemonNetworkListener(pokemonNetworkListener);
