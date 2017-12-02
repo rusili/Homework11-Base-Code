7fe765ee8393ed6439bbf5d89d571fbcfedf9f44
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2017-12-02 15:16:20 -0500

    added intent to itemviews

--- app/src/main/java/com/example/rusili/homework11/RecyclerViewPackage/view/PokemonViewHolder.java
+++ app/src/main/java/com/example/rusili/homework11/RecyclerViewPackage/view/PokemonViewHolder.java
@@ -1,12 +1,16 @@
 package com.example.rusili.homework11.RecyclerViewPackage.view;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.rusili.homework11.R;
 import com.example.rusili.homework11.RecyclerViewPackage.model.Pokemon;
+import com.example.rusili.homework11.detailscreen.view.PokemonDetailActivity;
 import com.example.rusili.homework11.pokedexActivity.model.objects.PokemonEntries;
 
 /**
@@ -15,15 +19,34 @@ import com.example.rusili.homework11.pokedexActivity.model.objects.PokemonEntrie
 
 public class PokemonViewHolder extends RecyclerView.ViewHolder{
 
+    ImageView sprite;
     TextView pokemonName;
 
     public PokemonViewHolder(View itemView) {
         super(itemView);
         pokemonName = itemView.findViewById(R.id.pokemon_name_text_view);
+        sprite = itemView.findViewById(R.id.sprite);
     }
 
     public void onBind(PokemonEntries pokemon){
+        final PokemonEntries pokemon1 = pokemon;
         Log.d("pokemon",pokemon.getPokemon_species().getName());
         pokemonName.setText(pokemon.getPokemon_species().getName());
+        Glide.with(itemView.getContext())
+                .load(pokemon.getPokemon_species().getUrl())
+                .into(sprite);
+
+        itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+           public void onClick(View view){
+                Intent intent = new Intent(itemView.getContext(), PokemonDetailActivity.class);
+                String name = pokemon1.getPokemon_species().getName();
+                intent.putExtra("pokemon",name);
+                itemView.getContext().startActivity(intent);
+            }
+                                    }
+        );
+
+
     }
 }
