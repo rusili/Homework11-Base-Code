227e4344172a03aad29ad3db8c1e7d266cd2cd3c
Taishawn Williams <twill4317@withs.org>
2017-12-02 11:09:14 -0500

    views

--- app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
@@ -3,7 +3,6 @@ package com.example.rusili.homework11.detailscreen.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
 import com.example.rusili.homework11.R;
 import com.example.rusili.homework11.detailscreen.model.Pokemon;
