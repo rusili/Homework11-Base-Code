eea7fc11d9dcad1bb880b2327041deed0af768a6
Chelsi Ampawa <chelsiampawa@gmail.com>
2017-12-02 18:09:40 -0500

    first commit

--- app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexFragment.java
+++ app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -28,11 +29,12 @@ public class PokedexFragment extends Fragment {
 		View view = inflater.inflate(R.layout.recycler_view_layout, container, false);
 		RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view_layout);
 
-		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+//		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+		GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
 		adapter = new PokemonAdapter();
 
 		recyclerView.setAdapter(adapter);
-		recyclerView.setLayoutManager(linearLayoutManager);
+		recyclerView.setLayoutManager(gridLayoutManager);
 
 		getPokedexList();
 
