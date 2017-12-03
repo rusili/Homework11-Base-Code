4bf6c17fc93d3014689ffdad9c1ee46046e57414
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2017-12-02 19:05:30 -0500

    centered names

--- app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexViewHolder.java
+++ app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexViewHolder.java
@@ -1,15 +1,19 @@
 package com.example.rusili.homework11.pokedexActivity.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.rusili.homework11.R;
+import com.example.rusili.homework11.detailscreen.view.PokemonDetailActivity;
 import com.example.rusili.homework11.pokedexActivity.model.Pokedex;
 import com.example.rusili.homework11.pokedexActivity.model.objects.PokemonEntries;
 
@@ -24,14 +28,17 @@ public class PokedexViewHolder extends ViewHolder {
     TextView name;
     ImageView pokemonPic;
     Context context;
+    CardView cardView;
 
 
     public PokedexViewHolder(View itemView) {
         super(itemView);
         name = (TextView) itemView.findViewById(R.id.pokedex_name);
+        cardView = (CardView) itemView.findViewById(R.id.cardview);
+
+
 
     }
-
 
     public void onBind(PokemonEntries pokedex) {
 
