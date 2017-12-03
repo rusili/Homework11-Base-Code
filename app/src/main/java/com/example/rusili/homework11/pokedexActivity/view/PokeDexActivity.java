b5f82ac44c2e6e8f608e614241834d0e467bb81c
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2017-12-02 22:09:46 -0500

    hid action bar

--- app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokeDexActivity.java
+++ app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokeDexActivity.java
@@ -16,6 +16,7 @@ public class PokeDexActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_poke_dex);
 
 
